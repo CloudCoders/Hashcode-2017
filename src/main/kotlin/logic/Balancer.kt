108b4538a072e32d5650777676d412d2a3494e71
Adriano Vega <adrianovega@hotmail.com>
2017-02-23 21:42:46 +0100

    solved bug

--- src/main/kotlin/logic/Balancer.kt
+++ src/main/kotlin/logic/Balancer.kt
@@ -12,8 +12,11 @@ class Balancer {
 
     private fun sortVideosByRequests(videos: List<Video>): List<Video> {
         videos.forEach { it.requests.sortedByDescending { it.requests } }
-        return videos.sortedByDescending { it.requests.get(0).requests }
+        return videos.sortedByDescending { it.requests[0].requests }
     }
+
+    private fun getVideosWithRequests(videos: List<Video>) : List<Video> =
+            videos.filter{!it.requests.isEmpty()}
 
     private fun canPutVideoInCacheServer(video: Video, cacheServer: CacheServer): Boolean =
             video.sizeMB <= cacheServer.size
@@ -22,7 +25,7 @@ class Balancer {
             endPoint.connections.keys.sorted().map { endPoint.connections[it]!! }
 
     fun balance(videos: List<Video>, cacheServers: List<CacheServer>): List<CacheServer> {
-        val sortedVideos = sortVideosByRequests(getLargeEnoughVideos(videos, cacheServers[0].size))
+        val sortedVideos = sortVideosByRequests(getLargeEnoughVideos(getVideosWithRequests(videos), cacheServers[0].size))
         var resultCaches = emptyList<CacheServer>()
 
         sortedVideos.forEach { video ->
